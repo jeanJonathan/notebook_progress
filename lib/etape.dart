62961f03a1ce36052354e5432f7dd3211d730a65
Jean Jonathan <jeanjonathank2@gmail.com>
2023-07-10 14:49:33 +0200

    implementation du modele etape

--- lib/etape.dart
+++ lib/etape.dart
@@ -1,1 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Etape {
+  final String etapeId;
+  final String name;
+  final String description;
+  final String niveauId;
+  final String sportId;
+
+  Etape({
+    required this.etapeId,
+    required this.name,
+    required this.description,
+    required this.niveauId,
+    required this.sportId,
+  });
+}
