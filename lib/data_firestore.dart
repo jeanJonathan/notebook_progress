bf3379d9985a7d2e7433835cbdd501b525bd9bbe
Jean Jonathan <jeanjonathank2@gmail.com>
2023-07-11 09:50:02 +0200

    Ajout du fichier data_firestore pour simuler et preparer les donnees de firebase

---
+++ lib/data_firestore.dart
@@ -0,0 +1,19 @@
+List<Map<String, dynamic>> simulateData() {
+  return [
+    {
+      'id': 1,
+      'description': 'Règles de sécurité de base',
+      'name': 'BEGINNER - 1.1',
+      'niveauRef': '/niveaux/1',
+      'sportRef': '/sports/1',
+    },
+    {
+      'id': 2,
+      'description': 'Description de l\'étape 2',
+      'name': 'Étape 2',
+      'niveauRef': '/niveaux/2',
+      'sportRef': '/sports/1',
+    },
+    // Ajoutez d'autres données d'étapes ici
+  ];
+}
