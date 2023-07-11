d237e0d3c0e2968adc6f1939bf6dccf22a8d6ca0
Jean Jonathan <jeanjonathank2@gmail.com>
2023-07-11 10:37:01 +0200

    Modification de la methode simulateData pour l'adapter ajouter le champ video

--- lib/data_firestore.dart
+++ lib/data_firestore.dart
@@ -1,18 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 List<Map<String, dynamic>> simulateData() {
   return [
     {
       'id': 1,
       'description': 'Règles de sécurité de base',
       'name': 'BEGINNER - 1.1',
-      'niveauRef': '/niveaux/1',
-      'sportRef': '/sports/1',
+      'niveauRef': FirebaseFirestore.instance.doc('/niveaux/1'),
+      'sportRef': FirebaseFirestore.instance.doc('/sports/1'),
+      'video': 'https://youtu.be/-HTkf1UXjiE',
     },
     {
       'id': 2,
       'description': 'Mise en place de l\'aile',
       'name': 'BEGGINER - 1.2',
-      'niveauRef': '/niveaux/2',
-      'sportRef': '/sports/1',
+      'niveauRef': FirebaseFirestore.instance.doc('/niveaux/1'),
+      'sportRef': FirebaseFirestore.instance.doc('/sports/1'),
+      'video': 'https://youtu.be/y4RfRN9V4tY',
     },
   ];
 }
