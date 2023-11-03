e6cf25eb68527e1963c2928a64655d98c77d4ffd
Jean Jonathan <jeanjonathank2@gmail.com>
2023-11-03 11:05:22 +0100

    amelioration de la liste des etapes pour l'ecran kitesurf

--- lib/etapes_screen.dart
+++ lib/etapes_screen.dart
@@ -163,102 +163,142 @@ class _EtapesScreenWingfoilState extends State<EtapesScreenWingfoil> {
 
 
 
-class EtapesScreenKitesurf extends StatelessWidget {
+class EtapesScreenKitesurf extends StatefulWidget {
+  @override
+  _EtapesScreenKitesurfState createState() => _EtapesScreenKitesurfState();
+}
+
+class _EtapesScreenKitesurfState extends State<EtapesScreenKitesurf> {
+  List<Etape> etapes = [];
+  String? userId;
+  List<Progression> progressions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    final user = FirebaseAuth.instance.currentUser;
+    userId = user?.uid;
+
+    FirebaseFirestore.instance.collection('etapes').snapshots().listen((snapshot) {
+      if (snapshot.docs.isNotEmpty) {
+        etapes = snapshot.docs.map((doc) => Etape.fromFirestore(doc)).where((etape) => etape.sportRef.id == '1').toList();
+        setState(() {});
+      }
+    });
+
+    FirebaseFirestore.instance
+        .collection('progression')
+        .where('userId', isEqualTo: userId)
+        .snapshots()
+        .listen((progressionSnapshot) {
+      if (progressionSnapshot.docs.isNotEmpty) {
+        progressions = progressionSnapshot.docs.map((doc) => Progression.fromFirestore(doc)).toList();
+        setState(() {});
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Liste des étapes',
+          'Étapes kitesurf',
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
         ),
         centerTitle: true,
-        //backgroundColor: Colors.blue, // Modifier la couleur de l'AppBar selon vos besoins
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('etapes').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Text('Une erreur s\'est produite');
-          }
+      body: ListView.builder(
+        itemCount: etapes.length,
+        itemBuilder: (context, index) {
+          Etape etape = etapes[index];
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+          bool dejaValidee = progressions.any((progression) => progression.etapeRef == etape.etapeId);
+          bool estVerouillee = (progressions.length + 1) <= index;
 
-          List<QueryDocumentSnapshot> documents = snapshot.data!.docs;
-          List<Etape> etapes = documents.map((doc) => Etape.fromFirestore(doc)).toList();
-          // On filtre les étapes dont sportRef est égal à 1
-          etapes = etapes.where((etape) => etape.sportRef.id == '1').toList();
-          return ListView.builder(
-            itemCount: etapes.length,
-            itemBuilder: (context, index) {
-              Etape etape = etapes[index];
-              return InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => EtapeDetailScreen(etape: etape, etapeId: etape.etapeId,),
-                    ),
-                  );
-                },
-                child: Container(
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        spreadRadius: 1,
-                        blurRadius: 5,
-                        offset: Offset(0, 2), // Changement de l'ombre selon vos préférences
-                      ),
-                    ],
+          return InkWell(
+            onTap: () {
+              if (estVerouillee) {
+                HapticFeedback.heavyImpact();
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('Cette étape est verrouillée.'),
+                  behavior: SnackBarBehavior.floating,
+                  backgroundColor: Colors.grey,
+                  duration: Duration(milliseconds: 500),
+                ));
+              } else {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EtapeDetailScreen(etape: etape, etapeId: etape.etapeId),
                   ),
-                  child: ListTile(
-                    leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
+                );
+              }
+            },
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              decoration: BoxDecoration(
+                color: estVerouillee ? Colors.grey[200] : Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: ListTile(
+                leading: Container(
+                  width: 56,
+                  height: 56,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: estVerouillee ? Colors.red : Colors.green,
+                  ),
+                  child: Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(25),
                       child: Image.asset(
                         'assets/kite.jpg',
-                        width: 56,
-                        height: 56,
+                        width: 50,
+                        height: 50,
                         fit: BoxFit.cover,
                       ),
                     ),
-                    title: Text(
-                      etape.name,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    subtitle: Text(
-                      etape.description,
-                      style: TextStyle(
-                        fontSize: 14,
-                      ),
-                    ),
-                    trailing: Icon(
-                      Icons.arrow_forward,
-                      color: Colors.grey,
-                    ),
                   ),
                 ),
-              );
-            },
+                title: Text(
+                  etape.name,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(
+                  etape.description,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+                trailing: estVerouillee
+                    ? Icon(Icons.lock, color: Colors.red)
+                    : (dejaValidee ? Icon(Icons.lock_open, color: Colors.green) : Icon(Icons.arrow_forward, color: Colors.black)),
+              ),
+            ),
           );
         },
       ),
     );
   }
 }
+
 
 
 class EtapesScreenSurf extends StatelessWidget {
