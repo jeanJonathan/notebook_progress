8544fb768bad4e45459d46e345be684e37f86a46
Jean Jonathan <jeanjonathank2@gmail.com>
2023-11-07 09:44:40 +0100

    Mise a jour du contenue de l'ecran parametre

--- lib/parametre_screen.dart
+++ lib/parametre_screen.dart
@@ -19,50 +19,71 @@ class _ParametresScreenState extends State<ParametresScreen> {
       body: ListView(
         children: [
           ListTile(
-            leading: Icon(Icons.star),
-            title: Text('Essayer la version premium gratuitement ⭐'),
+            leading: Text('⭐',
+              style: TextStyle(
+                fontSize: 26,),
+            ),
+            title: Text('Essayer la version premium gratuitement '),
             onTap: () {
               // Action lorsque l'utilisateur clique sur "Essayer la version premium gratuitement"
             },
           ),
           ListTile(
-            leading: Icon(Icons.account_circle),
-            title: Text('Mon profil 👤'),
+            leading: Text('👤',
+              style: TextStyle(
+                fontSize: 26,),
+            ),
+            title: Text('Mon profil '),
             onTap: () {
               // Action lorsque l'utilisateur clique sur "Mon profil"
             },
           ),
           ListTile(
-            leading: Icon(Icons.flag),
-            title: Text('Objectifs 🚩'),
+            leading: Text('🚩',
+              style: TextStyle(
+                fontSize: 26,),
+            ),
+            title: Text('Objectifs'),
             onTap: () {
               // Action lorsque l'utilisateur clique sur "Objectifs"
             },
           ),
           ListTile(
-            leading: Icon(Icons.lock),
-            title: Text('Centre de confidentialité 🔒'),
+            leading: Text('🔒',
+              style: TextStyle(
+                fontSize: 26,),
+            ),
+            title: Text('Centre de confidentialité'),
             onTap: () {
               // Action lorsque l'utilisateur clique sur "Centre de confidentialité"
             },
           ),
           ListTile(
-            leading: Icon(Icons.privacy_tip),
-            title: Text('Partage et vie privée 🙈'),
+            leading: Text('🙈',
+              style: TextStyle(
+                fontSize: 26,),
+            ),
+            title: Text('Partage et vie privée '),
             onTap: () {
               // Action lorsque l'utilisateur clique sur "Partage et vie privée"
             },
           ),
           ListTile(
-            leading: Icon(Icons.help),
-            title: Text('Aide ❓'),
+            leading: Text('❓',
+              style: TextStyle(
+                fontSize: 26,),
+            ),
+            title: Text('Aide'),
             onTap: () {
               // Action lorsque l'utilisateur clique sur "Aide"
             },
           ),
           ListTile(
-            leading: Icon(Icons.exit_to_app),
-            title: Text('Déconnexion 🚪'),
+            leading: Text('🚪',
+              style: TextStyle(
+                fontSize: 26,),
+            ),
+            title: Text('Déconnexion'),
             onTap: _deconnexion, // Appel de la fonction de déconnexion
           ),
         ],
