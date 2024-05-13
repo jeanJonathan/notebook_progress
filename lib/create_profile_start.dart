9bb831511455b5cff5cf9ba064afe70a846776c8
Jean Jonathan <jeanjonathank2@gmail.com>
2024-05-13 11:32:54 +0200

    implementation de l'ecran de creation de profile

--- lib/create_profile_start.dart
+++ lib/create_profile_start.dart
@@ -5,49 +5,73 @@ class CreateProfileStart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40.0),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0), // Ajusté pour un meilleur espace latéral
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center, // Assure que tout est centré
                 children: <Widget>[
+                  SizedBox(height: 100),
                   Text(
-                    'Rejoignez Ocean Adventure',
+                    'Bienvenue chez Ocean Adventure',
                     style: TextStyle(
-                      fontSize: 24,
+                      fontSize: 40, // Taille augmentée pour plus de présence
                       fontWeight: FontWeight.bold,
+                      color: Color(0xFF64C8C8), // Couleur pour écho à la marque Ocean Adventure
                     ),
                     textAlign: TextAlign.center,
                   ),
-                  SizedBox(height: 20),
+                  SizedBox(height: 12),
                   Text(
-                    'Gerez votre profil pour ',
-                    style: TextStyle(fontSize: 16),
+                    'Commencez par gérer votre profil pour une expérience personnalisée.',
+                    style: TextStyle(
+                      fontSize: 18, // Légère augmentation de la taille pour une meilleure lisibilité
+                      color: Colors.grey[700], // Couleur douce pour le texte explicatif
+                    ),
                     textAlign: TextAlign.center,
                   ),
-                  SizedBox(height: 50),
-                  Image.asset('assets/welcome_image.jpg'), // Assurez-vous d'avoir une image correspondante dans le dossier assets
-                  SizedBox(height: 50),
+                  SizedBox(height: 30),
+                  Image.asset(
+                    'assets/welcome_image.jpg',
+                    width: MediaQuery.of(context).size.width * 0.8, // Utilise 80% de la largeur de l'écran
+                  ),
+                  SizedBox(height: 40),
                   ElevatedButton(
                     onPressed: () {
                       // Logique pour naviguer vers le formulaire de création de compte
+                      Navigator.of(context).pushNamed('/createProfileForm');
                     },
-                    child: Text('Démarrer'),
+                    child: Text(
+                      'Démarrer',
+                      style: TextStyle(fontSize: 18,color: Colors.white),
+                    ),
                     style: ElevatedButton.styleFrom(
-                      minimumSize: Size(double.infinity, 50), // largeur infinie, hauteur de 50
+                      backgroundColor: Color(0xFF64C8C8), // Couleur de bouton qui se distingue
+                      minimumSize: Size(double.infinity, 60), // Bouton plus grand pour une meilleure touchabilité
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12), // Boutons avec coins arrondis
+                      ),
                     ),
                   ),
                   TextButton(
                     onPressed: () {
-                      // Logique pour naviguer vers l'écran de connexion
+                      // Logique pour revenir à l'écran précédent ou à l'écran de connexion
+                      Navigator.of(context).pop(); // Retour à l'écran précédent
                     },
-                    child: Text('Je ne souhaite pas maintenant'),
+                    child: Text(
+                      'Je ne souhaite pas maintenant',
+                      style: TextStyle(
+                        color: Colors.grey[700], // Une couleur neutre mais visible
+                      ),
+                    ),
                   ),
+                  SizedBox(height: 20),
                 ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
